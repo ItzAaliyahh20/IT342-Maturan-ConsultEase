23ae3e52f96a8458fc6bf2cc20ec528469a344db
ItzAaliyahh20 <maturanfrances123@gmail.com>
2026-03-07 01:50:55 +0800

    feature(backend): Springboot setup

---
+++ backend/consultease/src/main/java/edu/cit/maturan/consultease/ConsulteaseApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.maturan.consultease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulteaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulteaseApplication.class, args);
+	}
+
+}
---
+++ backend/consultease/src/test/java/edu/cit/maturan/consultease/ConsulteaseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.maturan.consultease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulteaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
