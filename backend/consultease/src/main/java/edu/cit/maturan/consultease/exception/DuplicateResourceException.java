61807731d549199652cae7555be5309502082723
ItzAaliyahh20 <maturanfrances123@gmail.com>
2026-05-09 16:48:52 +0800

    refactor(backend): remove old layered architecture

--- backend/consultease/src/main/java/edu/cit/maturan/consultease/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package edu.cit.maturan.consultease.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
--- backend/consultease/src/main/java/edu/cit/maturan/consultease/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.cit.maturan.consultease.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
